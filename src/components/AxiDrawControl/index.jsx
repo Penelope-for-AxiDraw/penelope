6f9f81d7427b63534d159cd4bd57ce1d54f736c6
Shawn J <shontek@gmail.com>
2022-05-31 23:54:28 -0700

    Move AxiConnection logic into AxiDrawControl

--- src/components/AxiDrawControl/index.jsx
+++ src/components/AxiDrawControl/index.jsx
@@ -1,11 +1,18 @@
-import { useContext } from "react";
+import { useContext, useState } from 'react';
 import { store } from '../../providers/store';
-import AxiConnection from '../AxiConnection';
+// import AxiConnection from '../AxiConnection';
+import { ControlsContainer, InputContainer, InputsWrapper, StyledAxiConnection } from './styles';
+import { ClearBtn, IconButton, InputLabel, OutlineBtn, SessionInfoCont } from '../StyledUiCommon/styles';
+import { getFromLocalStorage, saveToLocalStorage } from '../../utils';
+import { NetworkWiredIcon } from '../Icons';
 
 const AxiDrawControl = (props) => {
   const { axiWebsocket, deviceName } = props;
   const globalState = useContext(store);
-  const { dispatch, state: { isConnected} } = globalState;
+  const { dispatch, state: { isConnected, axiAddress, axiConnectionError } } = globalState;
+  const penUp = true;
+  const RAISE = 'Raise Pen';
+  const LOWER = 'Lower Pen';
 
   const initDisconnect = () => {
     const warningCopy = {
@@ -37,12 +44,78 @@ const AxiDrawControl = (props) => {
     });
   }
 
+  const handleChangeInput = (e) => {
+    if (axiConnectionError) {
+      // clear the error…
+      dispatch({
+        type: 'SET_CONNECTION_ERROR',
+        payload: {
+          data: ''
+        },
+      });
+    }
+
+    const updatedaxiAddress = {
+      ...axiAddress,
+      [e.target.name]: e.target.value,
+    };
+
+    dispatch({
+      type: 'SET_AXI_ADDRESS',
+      payload: {
+        data: updatedaxiAddress
+      },
+    });
+  };
+
+  if (isConnected) {
+    return (
+      <StyledAxiConnection>
+        <div>
+          <p className="info">Connected to AxiDraw</p>
+          <SessionInfoCont>
+            <NetworkWiredIcon width={40} height={40} fill={'#4400A3'} />
+            <div className="specs">
+              {deviceName && <p>{deviceName}</p>}
+              <p>{axiAddress.host} : {axiAddress.port}</p>
+              <ClearBtn onClick={initDisconnect}>disconnect</ClearBtn>
+            </div>
+          </SessionInfoCont>
+          <ControlsContainer>
+            <InputLabel>Pen Controls</InputLabel>
+            <div className="button-group">
+              <OutlineBtn onClick={() => axiWebsocket.send('toggle')}>{penUp ? LOWER : RAISE }</OutlineBtn>
+              <OutlineBtn onClick={() => axiWebsocket.send('align')}>Align Pen</OutlineBtn>
+            </div>
+          </ControlsContainer>
+        </div>
+      </StyledAxiConnection>
+    );
+  }
+
   return (
-    <AxiConnection
-      deviceName={deviceName}
-      initDisconnect={initDisconnect}
-      axiWebsocket={axiWebsocket}
-    />
+    <StyledAxiConnection>
+      <div>
+        <p className="info">AxiDraw Connection</p>
+        <InputsWrapper>
+          <InputContainer fieldWidth={11.5}>
+            <InputLabel htmlFor="axi-ip-address">host ip address</InputLabel>
+            <input name="host" className="input-field" type="text" placeholder='000.000.000.000' onChange={handleChangeInput} value={axiAddress.host} />
+          </InputContainer>
+
+          <InputContainer fieldWidth={4.75}>
+            <InputLabel htmlFor="axi-port">port</InputLabel>
+            <input name="port" className="input-field" type="text" placeholder='0000' onChange={handleChangeInput} value={axiAddress.port} />
+          </InputContainer>
+        </InputsWrapper>
+
+        {axiConnectionError && <p className="input-field-error">{axiConnectionError}</p>}
+
+        <p className="smallText">
+          To begin plotting, enter the IP address and port of your Axi server. You’ll need to be running the server in the background. Click here to download the server and read the documentation.
+        </p>
+      </div>
+    </StyledAxiConnection>
   );
 };
 
---
+++ src/components/AxiDrawControl/styles.jsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components'
+// import { Button } from '../StyledUiCommon/styles';
+
+export const StyledAxiConnection = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0;
+  justify-content: space-between;
+
+  & .info {
+    margin-top: 0;
+  }
+`;
+
+export const InputContainer = styled.div`
+  input {
+    height: 1.875rem;
+    font-size: 1rem;
+    flex-grow: 1;
+    width: ${(props) => props.fieldWidth}rem;
+    margin-top: 0.375rem;
+    padding-left: 0.375rem;
+    border-radius: 2px;
+    border-width: 1px;
+    border-top-color: rgba(107, 0, 255, 0.32);
+    border-bottom-color: rgba(107, 0, 255, 0.32);
+    border-left-color: rgba(107, 0, 255, 0.32);
+    border-right-color: rgba(107, 0, 255, 0.32);
+
+    &:focus-visible {
+      outline-color: rgba(107, 0, 255, 0.64);
+      outline-width: 1px;
+      outline-style: inset;
+    }
+  }
+
+  &:nth-child(2) {   
+    padding-left: 0.5rem;
+  }
+`;
+
+export const InputsWrapper = styled.div`
+  display: flex;
+
+  & > p.field-error {
+    font-size: 48px;
+  }
+`;
+
+export const ControlsContainer = styled.div`
+  margin-top: 1.5rem;
+
+  & .button-group {
+    display: flex;
+    margin-top: 0.5rem;
+
+    button {
+      flex: 1 1 0;
+    }
+
+    button:not(:last-child) {
+      margin-right: 0.5rem;
+    }
+  }
+`;
